ee03c062c89e1eaf91d4e9ea3aad2e12d524e01a
Jafar Mchasa Akida <JMchfundi@gmail.com>
2022-06-29 15:46:47 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<body class="page-index">
+
+<!-- header -->
+<?php include "fragments/header.php";?>
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="hero d-flex align-items-center">
+    <div class="container">
+      <div class="row">
+        <div class="col-xl-4">
+          <h2 data-aos="fade-up">Solutions We Can Trust - We Apologies For The InCovinience</h2>
+          <blockquote data-aos="fade-up" data-aos-delay="100">
+            <p>We Are Sorting Things Out For This Site, Get In Touch info@tucode.co.ke 
+              <a href="https://wa.me/254759184063">wa.me/254759184063</a>
+            </p>
+          </blockquote>
+          <div class="d-flex" data-aos="fade-up" data-aos-delay="200">
+            <a href="https://wa.me/254759184063" class="btn-get-started">Get InTouch</a>
+            <a href="about-video.php" class="glightbox btn-watch-video d-flex align-items-center"><i class="bi bi-play-circle"></i><span>Watch Video</span></a>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </section><!-- End Hero Section -->
+
+</body>
+
+</html>
