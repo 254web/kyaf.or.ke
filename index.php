716be2800590f78515aa0a736784ca034ddd7834
Jafar Mchasa Akida <JMchfundi@gmail.com>
2022-06-29 15:58:50 +0300

    ce

--- index.php
+++ index.php
@@ -19,7 +19,6 @@
           </blockquote>
           <div class="d-flex" data-aos="fade-up" data-aos-delay="200">
             <a href="https://wa.me/254759184063" class="btn-get-started">Get InTouch</a>
-            <a href="about-video.php" class="glightbox btn-watch-video d-flex align-items-center"><i class="bi bi-play-circle"></i><span>Watch Video</span></a>
           </div>
 
         </div>
