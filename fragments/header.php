9c00da52a931d4d19be39b2e71671f06b1d7acc6
Jafar Mchasa Akida <JMchfundi@gmail.com>
2022-06-29 15:59:20 +0300

    ce

--- fragments/header.php
+++ fragments/header.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>TuCode Inc Solutions - Official Site</title>
+  <title>KYAF - Official Site</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
